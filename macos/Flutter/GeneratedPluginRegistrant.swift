d5a35b6ee2fbb1b51941e3b1f38013a625b898d0
kfaitc <137277425+kfaitc@users.noreply.github.com>
2024-01-06 08:51:31 +0700

    meng

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_inappwebview
+import file_selector_macos
+import flutter_inappwebview_macos
 import geolocator_apple
 import path_provider_foundation
 import printing
@@ -14,6 +15,7 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
